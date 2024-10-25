4ac68c0438712fa35b4b53eaebe77119f9df9d5b
Ty Fiero <tyfierodev@gmail.com>
2024-10-25 09:01:45 -0700

    install deps

---
+++ src/tool_use/scripts/_script_dependencies.py
@@ -0,0 +1,21 @@
+SCRIPT_DEPENDENCIES = {
+    "cal": [
+        "anthropic",
+        "google-auth-oauthlib",
+        "google-auth",
+        "google-api-python-client",
+        "pytz",
+        "tzlocal",
+        "rich"
+    ],
+    "make-obsidian-plugin": [
+        "rich",
+        "inquirer"
+    ],
+    "scriptomatic": [
+        "scriptomatic"
+    ],
+    "script1": [
+        # Add dependencies for script1
+    ]
+}
